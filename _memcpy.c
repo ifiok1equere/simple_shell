a9d28280c24138633cc3e1095dfce7ea5ed2e2e2
ifiok1equere <equereifiok1@gmail.com>
2023-04-22 21:49:50 +0100

    More Helper functions

--- _memcpy.c
+++ _memcpy.c
@@ -15,9 +15,8 @@ char *_memcpy(char *dest, char *src, size_t n)
 	unsigned int i;
 
 	if (dest == NULL || src == NULL)
-            return NULL;
+		return (NULL);
 	for (i = 0; i < n; i++)
-
 	{
 		dest[i] = src[i];
 	}
--- main.h
+++ main.h
@@ -9,9 +9,13 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-int exec_func(char *argv[]);
-ssize_t fetchline(char **lineptr, size_t *n, FILE *stream);
-ssize_t fetchline1(char **p, char *q, char *r, char *ending_buf, size_t *n);
+int _strlen(char *s);
+char *_strchr(char *s, char c);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
 char *_realloc(char *ptr, size_t old_size, size_t new_size);
 char *_memcpy(char *dest, char *src, size_t n);
+ssize_t fetchline(char **lineptr, size_t *n, FILE *stream);
+ssize_t fetchline1(char **p, char *q, char *r, char *ending_buf, size_t *n);
 #endif
