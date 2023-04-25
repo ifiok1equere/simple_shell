981bc22715649569147dbfdf6e8b1210efe93a4c
ifiok1equere <equereifiok1@gmail.com>
2023-04-25 20:55:45 +0100

    changes

--- errors.c
+++ errors.c
@@ -6,7 +6,9 @@
 void errors(char *argv)
 {
 	char *error = ": command not found\n";
+	char *shell = "./hsh: ";
 
+	write(2, shell, _strlen(shell));
 	write(2, argv, _strlen(argv));
 	write(2, error, _strlen(error));
 }
